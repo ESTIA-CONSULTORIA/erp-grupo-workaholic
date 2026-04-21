290045680524d32b8277c89c0882ec69e45ef750
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-04-20 19:50:06 -0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Habilitar CORS para el frontend
   app.enableCors({
     origin: ['https://erp-grupoworka.netlify.app', 'http://localhost:3000'],
     credentials: true,
   });
-
-  // Prefijo global para todas las rutas
   app.setGlobalPrefix('api/v1');
-
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
